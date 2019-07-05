9ba1adcb13f2192a5eb78f6d76df43d23dc6d17a
Bruno Russi Lautenschlager <brunoxd13@gmail.com>
2019-07-05 10:46:59 -0300

    fix: ajusted config file to use styled components

--- gatsby-config.js
+++ gatsby-config.js
@@ -11,6 +11,7 @@ module.exports = {
   },
   plugins: [
     `gatsby-transformer-remark`,
+    `gatsby-plugin-styled-components`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
